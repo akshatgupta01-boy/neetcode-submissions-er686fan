19d41a9319c3ee3ebbf75d8409651b0ef4ed1206
Akshat Gupta <akshatgupta151593@gmail.com>
2026-05-09 20:37:23 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxProfit = 0;
+        int bestBuy = prices[0];
+        for(int i=0;i<prices.size();i++){
+            maxProfit = max(maxProfit, prices[i] - bestBuy);
+            bestBuy = min(bestBuy, prices[i]);
+        }
+        return maxProfit;
+    }
+};

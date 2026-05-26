aefa80bf81b03c5954e83961edbb53a03b3d320c
Akshat Gupta <akshatgupta151593@gmail.com>
2026-05-26 12:53:09 +0530

    Add: palindrome-partitioning - submission-0

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-0.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<string>> ans;
+    vector<string> path;
+
+    bool isPalindrome(string &s, int i, int j){
+        while(i < j){
+            if(s[i] != s[j]) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    void solve(string &s, int index){
+        if(index == s.length()){
+            ans.push_back(path);
+            return;
+        }
+        for(int i = index; i < s.length(); i++){
+            if(isPalindrome(s, index, i)){
+                path.push_back(s.substr(index, i - index + 1));
+                solve(s, i + 1);
+                path.pop_back();
+            }
+        }
+    }
+
+    vector<vector<string>> partition(string s) {
+        solve(s, 0);
+        return ans;
+    }
+};

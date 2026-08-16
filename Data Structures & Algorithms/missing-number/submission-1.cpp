e18a34c6fe55b2def60811c980ee5d86d9113f9d
Akshat Gupta <akshatgupta151593@gmail.com>
2026-08-16 22:50:08 +0530

    Add: missing-number - submission-1

---
+++ Data Structures & Algorithms/missing-number/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int ans = nums.size();
+        for(int i=0;i<nums.size();i++){
+            ans ^= i;
+            ans ^= nums[i];
+        }
+        return ans;
+        // int n = nums.size();
+        // int expected = n * (n + 1) / 2;
+        // int actual = 0;
+        // for(int num : nums){
+        //     actual += num;
+        // }
+        // return expected - actual;
+    }
+};
